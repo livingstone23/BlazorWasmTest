79c0743cd7de2db7909059fbdcb9c1cd743dd88b
Livingstone Cano <lcano@hypernovalabs.com>
2021-06-16 13:11:01 -0600

    Mejora, registro de clase

---
+++ BlazorWasmTest/Shared/Dependencies/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmTest.Shared.Dependencies
+{
+    public class Movies
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
